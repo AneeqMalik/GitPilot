a9aefd5ffd0ebfcf25b2dd12b917232c7fa01a56
AneeqMalik <aneeqfayyazkarimmalik@gmail.com>
2026-06-07 18:04:04 +0500

    build: remove dev PowerShell scripts and redundant publishing guide

--- pr-dashboard/fix-icon.ps1
+++
@@ -1,8 +0,0 @@
-[Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
-$image = [System.Drawing.Image]::FromFile("D:\extensions\GitPilot\pr-dashboard\icons\icon128.png")
-$image.Save("D:\extensions\GitPilot\pr-dashboard\icons\icon128_tmp.png", [System.Drawing.Imaging.ImageFormat]::Png)
-$image.Dispose()
-Remove-Item "D:\extensions\GitPilot\pr-dashboard\icons\icon128.png" -Force
-Rename-Item "D:\extensions\GitPilot\pr-dashboard\icons\icon128_tmp.png" "icon128.png" -Force
-Copy-Item "D:\extensions\GitPilot\pr-dashboard\icons\icon128.png" "D:\extensions\GitPilot\pr-dashboard\icons\icon48.png" -Force
-Copy-Item "D:\extensions\GitPilot\pr-dashboard\icons\icon128.png" "D:\extensions\GitPilot\pr-dashboard\icons\icon16.png" -Force
